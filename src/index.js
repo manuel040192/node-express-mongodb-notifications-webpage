6e956915c14c66b8b6331a3731198e6a2514e8a6
Manuel Arce <manuel_040192@hotmail.com>
2025-02-06 16:47:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import notificationRoutes from "./routes/notifications.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
 app.use(express.json());
 
 mongoose
